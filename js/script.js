8fec507abb36f95c9f8eb9c16d092b10c8a83eb0
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-04-03 14:15:46 +0100

    Reset btn and rng color for painting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-//random for rgb of colors
 const btn = document.querySelector('#btn-grids');
+const cleanCanvas = document.querySelector('#btn-clear');
 const sketchGrid = document.querySelector('.sketch-grid');
+const btnColors = document.querySelector('#btn-randomColor');
+let color = "";
+
 let gridsToCreate = 0;
 
 //sizes of the grid for calculations of how many grids we create
@@ -8,7 +11,8 @@ var height = sketchGrid.clientHeight;
 var width = sketchGrid.clientWidth;
 
 btn.addEventListener('click', getGridNumber);
-
+cleanCanvas.addEventListener('click', cleanGrid);
+btnColors.addEventListener('click', generateColors);
 
 function getGridNumber() {
     resetGrid();
@@ -39,4 +43,24 @@ function resetGrid() {
 
 function changeBackground() {
     this.classList.add('drawing');
+    if(color !== "") {
+        this.style.backgroundColor = `rgb(${color})`;
+    }
+}
+
+function cleanGrid() {
+    const gridElements = document.querySelectorAll('.drawing');
+    gridElements.forEach(grid => grid.classList.remove('drawing'));
+}
+
+function randomizeRGB() {
+    return Math.round(Math.random() * 256);
+}
+
+function generateColors() {
+    let r = randomizeRGB();
+    let g = randomizeRGB();
+    let b = randomizeRGB();
+
+    color = r + " " + g + " " + b; 
 }
