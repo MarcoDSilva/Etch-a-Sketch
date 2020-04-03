67d06f15546e90cce165ca469534bd53dee473e3
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-04-03 12:11:50 +0100

    Project working. Now needing to add some colors and maybe extra options

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-//create a 16x16 grid here
-//put them inside div sketchgrid
-
-//btn to reset/clear grid
-//popup or other to create a new grid with X squares~
-
 //random for rgb of colors
-
 const btn = document.querySelector('#btn-grids');
 const sketchGrid = document.querySelector('.sketch-grid');
 let gridsToCreate = 0;
@@ -16,23 +9,34 @@ var width = sketchGrid.clientWidth;
 
 btn.addEventListener('click', getGridNumber);
 
+
 function getGridNumber() {
     resetGrid();
-    gridsToCreate = parseInt(prompt("Num of squares? 100 x 100, 10x10, etc.", 16));
-    createDivsOntoGrid(gridsToCreate);
+    gridsToCreate = parseInt(prompt("Num of squares 'till max of 80. (Represents 80x80)", 16)); 
+
+    if(gridsToCreate <= 80) {
+        createDivsOntoGrid(gridsToCreate);
+    }  else {
+        createDivsOntoGrid(80);
+    }
 }
 
 function createDivsOntoGrid(number) {
-    for (let rows = 0; rows < number; rows++) {
-        for (let columns = 0; columns < number; columns++) {
-            const div = document.createElement('div');
-            sketchGrid.appendChild(div);
-        }
+    sketchGrid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+
+    for (let rows = 0; rows < number * number; rows++) {
+        const div = document.createElement('div');
+        div.addEventListener('mouseover', changeBackground);
+        sketchGrid.appendChild(div);
     }
+    
 }
 
 function resetGrid() {
     sketchGrid.innerHTML = "";
-    gridsToCreate = 0;
-    
+    gridsToCreate = 0;    
+}
+
+function changeBackground() {
+    this.classList.add('drawing');
 }
